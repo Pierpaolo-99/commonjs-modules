43c5d298b7e0db67c5790a0134029aa2485f77ec
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-03-10 15:28:10 +0100

    first commit

---
+++ names.js
@@ -0,0 +1,15 @@
+/* traccia :
+Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
+Esporta la funzione dal file. */
+
+function names (firstName,lastName){
+    let name = {
+        firstName: firstName,
+        lastName: lastName
+    }
+    return name
+}
+
+module.exports = names;
+
+console.log(names('Pierpaolo', 'Dellioio'));
